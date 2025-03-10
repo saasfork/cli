fc19de971c991eaf9947b34f531f9737de16d100
Saas Fork <contact@saasfork.io>
2025-03-10 12:23:56 +0100

    feat(dev) Ajouter des constantes pour le chemin de fichier et le répertoire dans le module d'installation

--- src/utils/installer.js
+++ src/utils/installer.js
@@ -1,6 +1,10 @@
+import { fileURLToPath } from 'url';
 import { execSync } from "child_process";
 import fs from "fs";
 import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function createNuxtProject(projectName) {
   console.log(`Creating project "${projectName}"...`);
